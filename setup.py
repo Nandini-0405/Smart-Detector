11a93124ef2443050e7230deceac7ff4be15a3fa
Nandini-0405 <nandinisingh3563@gmail.com>
2024-11-26 14:41:50 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,5 +2,8 @@ from setuptools import setup c
 from Smart_detector import __version__
 setup(
   name='Smart_detector',
-  version=__version__
-  url="
+  version=__version__,
+  url="https://github.com/Nandini-0405/Smart-Detector",
+  author="Nandini Singh",
+  py_modules=['Smart_detector'],
+)
